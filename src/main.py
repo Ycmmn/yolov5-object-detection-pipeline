cf8653739a630535a52f01542b923e79cb6b4fb6
Yasaman <156900175+Ycmmn@users.noreply.github.com>
2025-08-24 00:10:40 +0000

    add src/main.py

--- src/main.py
+++ src/main.py
@@ -14,3 +14,7 @@ torch.backends.cudnn.benchmark = True      # increase FPS speed
 # the labels that we want to be in YOLO
 WANT = {'person','car','truck','bus','motorcycle','bicycle','dog','cat','bird','horse','cow','sheep'}
 
+def load_model():
+    # load the model and set it up simply
+    modelmodel = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).to(DEVICE)
+
